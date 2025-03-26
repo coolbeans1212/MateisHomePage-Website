af5848377b78d8ebbd8dd7c4366f79975df84f62
coolbeans1212 <matei@mateishome.page>
2025-03-26 18:06:54 +0000

    trim empty whitespace from the end

--- api/misc/GetGBMessage.php
+++ api/misc/GetGBMessage.php
@@ -4,6 +4,7 @@ $message = ["message" => ''];
 for ($i = rand(1, 100); $i > 0; $i--) {
     $message["message"] .= 'meow ';
 }
+$message["message"] = trim($message["message"]);
 echo json_encode($message);
 
 ?>
